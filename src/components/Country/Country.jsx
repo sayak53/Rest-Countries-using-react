d178e6ac98cd8e6dd88cec6816fb2ed2674496c3
sayak53 <sayakchakraborty928@gmail.com>
2024-10-19 20:12:52 +0530

    added some styles and logic

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,10 +1,12 @@
 import './Country.css'
 const Country = ({country}) => {
-    const{name,flags} = country
+    const{name,flags,population,area} = country
     return (
         <div className='country'>
             <h3>Name: {name.common}</h3>
             <img src={flags.png} alt="" />
+            <h3>Population: {population}</h3>
+            <h3>Population: {area}</h3>
         </div>
     )
 };
